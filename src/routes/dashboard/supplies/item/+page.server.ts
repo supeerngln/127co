9e941526400417a367b0fb658682122db997c573
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-01-08 01:52:16 +0800

    Refactor database import in page.server.ts files

--- src/routes/dashboard/supplies/item/+page.server.ts
+++ src/routes/dashboard/supplies/item/+page.server.ts
@@ -1,7 +1,6 @@
-import { Database } from "$lib/server/database";
+import db from "$lib/server/database";
 
 export async function load() {
-  const db = await Database.get();
   const items = await db.query("SELECT * FROM ITEM");
 
   return {
--- src/routes/dashboard/supplies/transactions/+page.server.ts
+++ src/routes/dashboard/supplies/transactions/+page.server.ts
@@ -1,7 +1,6 @@
-import { Database } from "$lib/server/database";
+import db from "$lib/server/database";
 
 export async function load() {
-  const db = await Database.get();
   const transactions = await db.query("SELECT * FROM TRANSACTION_HISTORY");
 
   return {
