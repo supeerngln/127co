a13064e4b2f09624a5a92ab772f36a4de40e7b99
nasch <111645489+charrienacor@users.noreply.github.com>
2024-01-11 00:45:05 +0800

    added marketing folder on lib/components

---
+++ src/lib/components/marketing/Table.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    export let headers: Array<string> = [];
+    export let rows: Array<Record<string, any>> = [];
+    export let primaryKey: string;
+  
+    export let handleEdit: (id: number) => void;
+    export let handleDelete: (id: number) => void;
+  </script>
+  
+  <div class="flex mt-4 mb-4 flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+      <div class="p-1.5 min-w-full inline-block align-middle">
+        <div class="overflow-hidden border-outline">
+          <table
+            class="min-w-full divide-y bg-tablebg dark:divide-gray-700 border-outline"
+          >
+            <thead>
+              <tr>
+                {#each headers as header (header)}
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >{header}</th
+                  >
+                {/each}
+                <!-- <th
+                  scope="col"
+                  class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                  >Actions</th
+                > -->
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+              {#each rows as row (row)}
+                <tr class="hover:bg-tableh dark:hover:bg-gray-700">
+                  {#each Object.values(row) as value (value)}
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
+                      >{value}</td
+                    >
+                  {/each}
+                  <!-- <td
+                    class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium"
+                  >
+                    <button
+                      on:click={() => handleEdit(row[primaryKey])}
+                      type="button"
+                      class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      >Edit</button
+                    >&nbsp;&nbsp;&nbsp;
+                    <button
+                      on:click={() => handleDelete(row[primaryKey])}
+                      type="button"
+                      class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-active hover:text-hactive disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      >Delete</button
+                    >
+                  </td> -->
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  
