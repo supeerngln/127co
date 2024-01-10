526bb9e4ec7610383c94ab4ab4afa28c75db184e
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-01-10 15:13:33 +0800

    Update database schema and insert data

--- database/projects.sql
+++ database/projects.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS Timeline (
     Timeline_StartDate DATE NOT NULL,
     Timeline_ExpectedFinishDate DATE NOT NULL,
     Timeline_FinishDate DATE NOT NULL,
-    Timeline_DaysLeft INT NOT NULL,
     PRIMARY KEY (Timeline_ID)
 );
 
@@ -26,11 +25,9 @@ CREATE TABLE IF NOT EXISTS Project (
     Project_Status VARCHAR(50) NOT NULL,
     Project_Timeline_ID INT NOT NULL,
     Project_Team_ID INT NOT NULL,
-    Project_Contract_ID INT NOT NULL,
     PRIMARY KEY (Project_ID),
     FOREIGN KEY (Project_Timeline_ID) REFERENCES Timeline(Timeline_ID),
-    FOREIGN KEY (Project_Team_ID) REFERENCES Team(Team_ID),
-    FOREIGN KEY (Project_Contract_ID) REFERENCES Contract(Contract_ID)
+    FOREIGN KEY (Project_Team_ID) REFERENCES Team(Team_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Software (
@@ -38,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Software (
     Software_Version VARCHAR(50) NOT NULL,
     Software_Type VARCHAR(50) NOT NULL,
     Software_OS VARCHAR(50) NOT NULL,
-    Software_Size INT NOT NULL,
+    Software_Size VARCHAR(50) NOT NULL,
     Software_Publisher VARCHAR(50) NOT NULL,
     Software_License VARCHAR(50) NOT NULL,
     PRIMARY KEY (Software_Name, Software_Version)
@@ -53,37 +50,65 @@ CREATE TABLE IF NOT EXISTS Team_Software (
     FOREIGN KEY (Software_Name, Software_Version) REFERENCES Software(Software_Name, Software_Version)
 );
 
-INSERT INTO Timeline (Timeline_StartDate, Timeline_ExpectedFinishDate, Timeline_FinishDate, Timeline_DaysLeft) VALUES
-('2018-10-01', '2018-10-31', '2018-10-31', 0),
-('2018-10-01', '2018-11-28', '2018-11-28', 0),
-('2018-10-01', '2018-10-31', '2018-10-31', 0),
-('2018-10-01', '2018-10-30', '2018-10-30', 0),
-('2018-10-01', '2018-11-30', '2018-11-30', 0);
+INSERT INTO Timeline (Timeline_StartDate, Timeline_ExpectedFinishDate, Timeline_FinishDate) VALUES
+('2018-10-01', '2018-10-31', '2018-10-31'),
+('2018-10-01', '2018-11-28', '2018-11-28'),
+('2018-10-01', '2018-10-31', '2018-10-31'),
+('2018-10-01', '2018-10-30', '2018-10-30'),
+('2018-10-01', '2018-11-30', '2018-11-30'),
+('2018-10-01', '2018-10-31', '2018-10-31'),
+('2018-10-01', '2018-11-28', '2018-11-28'),
+('2018-10-01', '2018-10-31', '2018-10-31'),
+('2018-10-01', '2018-10-30', '2018-10-30'),
+('2018-10-01', '2018-11-30', '2018-11-30'),
+('2018-10-01', '2018-10-31', '2018-10-31');
 
 INSERT INTO Team (Team_Name, Team_Leader_ID) VALUES
-('Team 1', 20160001),
-('Team 2', 20170004),
-('Team 3', 20180006),
-('Team 4', 20170056),
-('Team 5', 20180025);
+('Alpha', 20170004),
+('Beta', 20230007),
+('Gamma', 20230012),
+('Delta', 20220001),
+('Epsilon', 20230007),
+('Zeta', 20170004),
+('Eta', 20230007),
+('Theta', 20230012),
+('Iota', 20220001),
+('Kappa', 20230007);
 
-INSERT INTO Project (Project_Name, Project_Description, Project_Type, Project_Budget, Project_Status, Project_Timeline_ID, Project_Team_ID, Project_Contract_ID) VALUES
-('Uno', 'Project 1 Description', 'Web Application', 100000, 'In Development', 1, 1, 1),
-('Dos', 'Project 2 Description', 'Mobile Application', 200000, 'For Presentation', 2, 2, 2),
-('Tenesis', 'Project 3 Description', 'Stand-alone Systems', 300000, 'For Deployment', 3, 3, 3),
-('Quatro', 'Project 4 Description', 'Web Application', 400000, 'For Monitoring', 4, 4, 4),
-('Senco', 'Project 5 Description', 'Mobile Application', 500000, 'Done', 5, 5, 5);
+INSERT INTO Project (Project_Name, Project_Description, Project_Type, Project_Budget, Project_Status, Project_Timeline_ID, Project_Team_ID) VALUES
+('Uno', 'Grading Management Application', 'Web Application', 500000, 'Done', 1, 1),
+('Dos', 'Mobile Application for Resource Management', 'Mobile Application', 500000, 'Done', 2, 2),
+('Tenesis', 'Stand-alone System for Medical Laboratories', 'Stand-alone Systems', 500000, 'Done', 3, 3),
+('Quatro', 'Note Taking Application', 'Web Application', 500000, 'Done', 4, 4),
+('Senco', 'Mobile Application for the Blind', 'Mobile Application', 500000, 'Done', 5, 5),
+('Hydra', 'E-Commerce Application', 'Web Application', 600000, 'In Development', 6, 6),
+('Helios', 'Smart Home Automation System', 'Mobile Application', 600000, 'For Presentation', 7, 7),
+('Aqua', 'Library Management Software', 'Stand-alone Systems', 300000, 'For Deployment', 8, 8),
+('Medicord', 'Medical Records Management System', 'Web Application', 200000, 'For Monitoring', 9, 9),
+('Traveler', 'Travel Companion Application', 'Mobile Application', 600000, 'Terminated', 10, 10),
+('Talk', 'Language Learning Application', 'Mobile Application', 600000, 'For Monitoring', 11, 10);
+
 
 INSERT INTO Software (Software_Name, Software_Version, Software_Type, Software_OS, Software_Size, Software_Publisher, Software_License) VALUES
-('Software 1', '1.0', 'Software 1 Type', 'Software 1 OS', 100, 'Software 1 Publisher', 'PDI'),
-('Software 2', '2.0', 'Software 2 Type', 'Software 2 OS', 200, 'Software 2 Publisher', 'LGPL'),
-('Software 3', '3.0', 'Software 3 Type', 'Software 3 OS', 300, 'Software 3 Publisher', 'Permissive'),
-('Software 4', '4.0', 'Software 4 Type', 'Software 4 OS', 400, 'Software 4 Publisher', 'Copy Left'),
-('Software 5', '5.0', 'Software 5 Type', 'Software 5 OS', 500, 'Software 5 Publisher', 'Proprietary');
+('CodeCraft', '3', 'Integrated Development Environment (IDE)', 'Windows, macOS, Linux', '300 MB', 'DevWorks Inc.', 'Proprietary'),
+('SecureShield', '2.5', 'Security Suite', 'Windows', '150 MB', 'CyberSec Labs', 'Commercial'),
+('DataSphere', '1.2', 'Data Analytics Tool', 'Windows, Linux', '500 MB', 'Analytix Corp.', 'Open Source'),
+('PixelPulse', '5.1', 'Graphic Design Software', 'macOS', '700 MB', 'DesignMasters Ltd.', 'Commercial'),
+('CloudConnect', '4', 'Cloud Management', 'Cross-platform', '200 MB', 'Cloudify Systems', 'Proprietary'),
+('NetEngine', '7.3', 'Networking Software', 'Windows, Linux', '400 MB', 'NetWorks Inc.', 'Commercial'),
+('AIForge', '2', 'Artificial Intelligence Platform', 'Linux', '600 MB', 'AITech Solutions', 'Open Source'),
+('MusicMatrix', '3.5', 'Audio Editing Software', 'Windows, macOS', '250 MB', 'SonicMasters Inc.', 'Commercial'),
+('HealthHub', '1', 'Healthcare Management Software', 'Web-based', '250 MB', 'HealthTech Innovations', 'Commercial'),
+('SwiftServe', '6.2', 'Web Server Software', 'Linux', '100 MB', 'WebSoft Ltd.', 'Open Source');
 
 INSERT INTO Team_Software (Team_ID, Software_Name, Software_Version) VALUES
-(1, 'Software 1', '1.0'),
-(2, 'Software 2', '2.0'),
-(3, 'Software 3', '3.0'),
-(4, 'Software 4', '4.0'),
-(5, 'Software 5', '5.0');
+(1, 'CodeCraft', '3'),
+(2, 'SecureShield', '2.5'),
+(3, 'DataSphere', '1.2'),
+(4, 'PixelPulse', '5.1'),
+(5, 'CloudConnect', '4'),
+(6, 'NetEngine', '7.3'),
+(7, 'AIForge', '2'),
+(8, 'MusicMatrix', '3.5'),
+(9, 'HealthHub', '1'),
+(10, 'SwiftServe', '6.2');
