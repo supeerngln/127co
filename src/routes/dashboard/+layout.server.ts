0d9283e85a6c8f070fb0c3d129ecdd57a8a2a717
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-01-10 15:13:42 +0800

    Update dashboard layout and page content

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -10,7 +10,7 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
   const role = cookies.get("role");
 
   let links = [
-    { href: "/", label: "Home", icon: "home" },
+    { href: "/", label: "Overview", icon: "home" },
     {
       href: "/dashboard/human_resources",
       label: "Human Resources",
