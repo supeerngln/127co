885fa0459b51cbc72ea31d294684f83701cd4a0c
Daniel David N. Bador <67200718+D4N13LxD4V1D@users.noreply.github.com>
2024-01-11 05:26:08 +0000

    Update project management dashboard (#41)
    * move projects, teams, and software to project management
    
    * Fix formatting
    
    * Refactored project management page to use DescriptiveLink component
    
    * Refactor team page server code and create team page svelte component
    
    * Update team breadcrumb text
    
    * Update team breadcrumb text
    
    * Fix formatting
    
    * Update breadcrumb links in software delete and edit pages
    
    * Update PageData to PageServerData in dashboard routes
    
    * Add member and remove member functionality
    
    * Fix formatting

---
+++ src/routes/dashboard/project_management/teams/[id]/delete/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+  import type { PageServerData } from "./$types";
+
+  export let data: PageServerData;
+</script>
+
+<div>
+  <Breadcrumb
+    items={[
+      { href: "/dashboard/project_management", text: "Project Management" },
+      { href: "/dashboard/project_management/teams", text: "Teams" },
+      {
+        href: `/dashboard/project_management/teams/${data.team.Team_ID}`,
+        text: data.team.Team_Name,
+      },
+      {
+        href: `/dashboard/project_management/teams/${data.team.Team_ID}/delete`,
+        text: `Delete ${data.team.Team_Name}`,
+      },
+    ]}
+  />
+
+  <div class="container">
+    <p class="mb-4">Are you sure you want to delete this team?</p>
+    <form method="POST">
+      <button type="submit">Yes</button>
+      <a href="/dashboard/project_management/teams/{data.team.Team_ID}">No</a>
+    </form>
+  </div>
+</div>
