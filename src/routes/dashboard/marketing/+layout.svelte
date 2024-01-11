460f3be99ede7cfd0974ba8b4ee641f59751baeb
nasch <111645489+charrienacor@users.noreply.github.com>
2024-01-11 09:39:25 +0800

    Add files via upload

---
+++ src/routes/dashboard/marketing/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { alerts } from "$lib/store";
+  import Alerts from "$lib/components/Alerts.svelte";
+</script>
+
+<Alerts data={$alerts} />
+<slot />
---
+++ src/routes/dashboard/marketing/[table]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Table from "$lib/components/marketing/Table.svelte";
+    import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+    import type { PageServerData } from "./$types";
+    import Tables from "$lib/tables";
+  
+    export let data: NonNullable<PageServerData>;
+  
+    $: rows = data["data"] as Array<Record<string, any>>;
+    $: table = data["table"] as string;
+  
+    // @ts-ignore
+    $: ({ name } = Tables[table]);
+  </script>
+  
+  <main class="w-full">
+    <Breadcrumb
+      items={[
+        { href: "/dashboard/marketing", text: "Marketing and Customer Acquisition" },
+        { href: `/dashboard/marketing/${table}`, text: name },
+      ]}
+    />
+    <a
+      href={`/dashboard/marketing/${table}/add`}
+      class="bg-buttonp rounded-lg z-4 border-2 w-40 border-outline p-3 mb-4 flex items-center hover:bg-buttonphover active:bg-buttonpactive"
+    >
+      <span class="text-3xl material-symbols-outlined mr-2"> add </span>
+      <div class="flex flex-col h-full">
+        <span>Add an Entry</span>
+      </div>
+    </a>
+    <Table {table} {rows} />
+  </main>
+  
