ccc292c865b7a764b9e9270b70005092c387f350
Ji Sison <jisison.dev@gmail.com>
2024-01-13 13:46:09 +0800

    initial commit

---
+++ src/routes/dashboard/bootcamp/courses_offered/[id]/edit_course/+page.server.ts
@@ -0,0 +1,97 @@
+import db from "$lib/server/database";
+import type { ResultSetHeader, RowDataPacket } from "mysql2";
+import type { PageServerLoad, Actions } from "./$types";
+import { redirect } from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ cookies, params }) => {
+
+
+
+
+  // All Employees
+  const [instructors] = await db.execute<RowDataPacket[]>(
+    `SELECT 
+      e.Employee_ID,
+      CONCAT(e.Employee_FirstName, ' ', e.Employee_LastName, ', ', p.Employee_HighestLevelOfEducation, ', ', j.Employee_Status, ', ', j.Employee_Shift, ' Shift') AS names
+    
+    FROM Employee e
+    INNER JOIN Job j ON e.Employee_ID = j.Employee_ID
+    INNER JOIN PDS p ON e.Employee_ID = p.Employee_ID
+    WHERE j.Job_Position = "Instructor"`
+  );
+
+
+  const [courses] = await db.execute<RowDataPacket[]>(
+    `SELECT Course_ID FROM Course_Offered`
+  );
+
+    
+ 
+  const employeeNames = instructors;
+  const courseID = courses;
+
+
+
+  return {
+    employeeNames, courseID
+  };
+};
+
+export const actions = { 
+  default: async ({ request, params }) => {
+    const data = await request.formData();
+    console.log(data.get("employeeID"));
+    console.log(data.get("courseID"));
+    console.log(data.get("courseName"));
+    console.log(data.get("courseInstructor"));
+    console.log(data.get("courseSchedule"));
+    console.log(data.get("courseDuration"));
+    console.log(data.get("courseCapacity"));
+    console.log(data.get("courseCategory"));
+
+    const employeeID = data.get("employeeID");
+    const courseID = data.get("courseID");
+    const courseName = data.get("courseName");
+    const courseInstructor = data.get("employeeID");
+    const courseSchedule = data.get("courseSchedule");
+    const courseDuration = data.get("courseDuration");
+    const courseCapacity = data.get("courseCapacity");
+    const courseCategory = data.get("courseCategory");
+
+
+    const [course_add] = await db.execute<ResultSetHeader[]>(
+      `INSERT INTO Course_Offered 
+        (Course_ID, Employee_ID, Course_Name, Course_Category, Course_Duration, Course_Capacity, Course_Schedule)
+        VALUES
+            ("${courseID}", 
+            "${employeeID}", 
+            "${courseName}", 
+            "${courseCategory}", 
+            "${courseDuration}", 
+            "${courseCapacity}", 
+            "${courseSchedule}")`
+
+    );
+
+    const [instructor_add] = await db.execute<ResultSetHeader[]>(
+        `INSERT INTO Instructor
+        (Employee_ID, Course_ID)
+        VALUES
+            ("${employeeID}", "${courseID}")`
+    );
+
+
+    // console.log(params.id);
+    
+    // const [enrollment] = await db.execute<RowDataPacket[]>(
+    //   `SELECT * FROM Course_Enrolled WHERE Enrollment_ID = ${data.get("enrollmentID")}`,
+    // );
+
+    console.log(course_add);
+    console.log(instructor_add);
+    throw redirect(302, `/dashboard/bootcamp/courses_offered`);
+  }
+
+} satisfies Actions;
+
+
---
+++ src/routes/dashboard/bootcamp/courses_offered/[id]/edit_course/+page.svelte
@@ -0,0 +1,193 @@
+<script lang="ts">
+    import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+    import type { PageServerData } from "./$types";
+    import Menu from "$lib/components/Menu.svelte";
+    export let data: PageServerData;
+    const { employeeNames, courseID } = data;
+    let names = employeeNames.map((employeeNames) => employeeNames.names)
+    let courseIDMap = courseID.map((courseID) => courseID.Course_ID)
+    
+
+    let courseName: string;
+    let selectedName:string;
+    let newcourseID: string;
+    let courseSched1: string;
+    let courseSched2: string;  
+    let courseSched3: string;
+    let courseSchedule: string;
+
+
+    function getEmployeeID(selectedName: string): string {
+        const employee = employeeNames.find((employeeNames) => employeeNames.names === selectedName);
+        return employee ? employee.Employee_ID : '';
+    }
+
+    function generateCourseID(courseName: string): string {
+        let newCourseID = '';
+        if (courseName) {
+            let baseID = courseName.substring(0, 3).toUpperCase();
+            let num = 100;
+            newCourseID = `${baseID}${num}`;
+            while (courseIDMap.includes(newCourseID)) {
+                num++;
+                newCourseID = `${baseID}${num}`;
+            }
+        }
+        return newCourseID;
+    }
+
+
+    $: {newcourseID = generateCourseID(courseName);}
+    $: courseSched2Formatted = new Date(`1970-01-01T${courseSched2}:00`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    $: courseSched3Formatted = new Date(`1970-01-01T${courseSched3}:00`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    $: courseSchedule = courseSched1 + " " + courseSched2Formatted + " - " + courseSched3Formatted;
+    
+</script>
+
+
+<main class="w-full">
+    <Breadcrumb items={[
+        { href: "/dashboard/bootcamp", text: "Bootcamp" },
+        { href: "/dashboard/bootcamp/courses_offered", text: "Courses Offered" },
+        { href: `/dashboard/bootcamp/courses_offered/add`, text: "Add Course"}
+    ]}/>
+
+    <h3>Add new course to the bootcamp</h3>
+    <form method="POST" class="mt-6">
+        <label for="newcourseID">
+            Course ID:
+        </label>
+        <br>
+        <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="text" 
+            name="newcourseID" 
+            id="newcourseID"
+            bind:value={newcourseID}
+            required 
+            disabled
+            
+        >
+        <input type="hidden" name="courseID" id="courseID" value={newcourseID}>
+        <br>
+        <label for="courseName">
+            Course Name:
+        </label>
+        <br>
+        <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="text" 
+            name="courseName" 
+            id="courseName" 
+            placeholder="Enter course name" 
+            bind:value={courseName}
+            minlength="4"
+            required 
+            
+        >
+        <br>
+        <label for="courseCategory">
+            Course Category:
+        </label>
+        <br>
+        <select 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            name="courseCategory" 
+            id="courseCategory" 
+            required 
+        >
+            <option value="">Select a category</option>
+            <option value="Soft Skills">Soft Skills</option>
+            <option value="Technical Skills">Technical Skills</option>
+        </select>
+        <br>
+        <label for="courseSchedule">
+            Course Schedule (Weekly Schedule and Time slot):
+        </label>
+        <br>
+        <select 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            name="courseSched1" 
+            id="courseSched1" 
+            bind:value={courseSched1}
+            required 
+        >
+            <option value="">Select a weekly schedule</option>
+            <option value="TThSat">TThSat</option>
+            <option value="MWF">MWF</option>
+        </select>
+        from
+        <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="time" 
+            name="courseSched2" 
+            id="courseSched2" 
+            bind:value={courseSched2}
+            required 
+        />
+        to
+        <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="time" 
+            name="courseSched3" 
+            id="courseSched3" 
+            bind:value={courseSched3}
+            required 
+        />
+        <input type="hidden" name="courseSchedule" id="courseSchedule" value={courseSchedule}>
+
+        <br>
+        <label for="courseInstructor">
+            Course Instructor:
+        </label>
+        <br>
+        <select
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2 w-1/2"
+            id="courseInstructor"
+            name="courseInstructor"
+            bind:value={selectedName}
+            
+            style="margin-bottom: 10px;"
+        >
+            <option value="">Select an employee</option>
+            {#each names as name}
+            <option value={name}>{name}</option>
+            {/each}
+         </select>
+         <input id="employeeID" name="employeeID"
+         class="hidden"
+         value={getEmployeeID(selectedName)}> 
+        <br>
+        <label for="courseDuration">
+            Course Duration (months): 
+            <br>
+            <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="number" 
+            name="courseDuration" 
+            id="courseDuration" 
+            min="1"
+            
+            placeholder="Enter how many months" 
+            required 
+        >
+        <br>
+        <label for="courseSlots">
+            Course Capacity:
+        </label>
+        <br>
+        <input 
+            class="text-sm border-2 rounded-full border-gray-400 p-1 pl-2 pr-2"
+            type="number" 
+            name="courseCapacity" 
+            id="courseCapacity" 
+            min="1"
+            placeholder="Enter course slots" 
+            required 
+        >
+        <br>
+        <button type="submit">
+            Add Course
+        </button>
+    </form>
+</main>
