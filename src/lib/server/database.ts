b03dd788fa953306850f94cde9a9734ddb720a01
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-01-14 14:36:15 +0800

    revert password to empty string

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "",
   database: "CS127",
   multipleStatements: true,
 });
